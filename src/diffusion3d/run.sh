dedc45dffe5e68c99bf07a573c4a2210c77987af
Naoya Maruyama <nmaruyama@riken.jp>
2014-10-29 18:23:54 -0700

    Measurement driver script

---
+++ src/diffusion3d/run.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+p=$1
+for i in $(seq 5); do
+    echo Trial $i
+    for exe in $(find . -name $p);  do
+        echo "$exe"
+        $exe > out
+        if grep Accuracy out  | grep 1.96 |grep "e-07"; then
+            echo $exe >> results.$p.$i
+            cat out >> results.$p.$i
+        else
+            echo fail: $(grep Accuracy out)
+        fi
+    done
+    echo ""
+done
